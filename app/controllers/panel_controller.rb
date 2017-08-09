fcae1283456ba6f4a5c477f33ab2c56964b0dbbe
Aldo <aldompe95@gmail.com>
2017-08-09 21:50:28 +0000

    Add the sum of all products

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -3,6 +3,10 @@ class PanelController < ApplicationController
   def index
     @brands = Brand.all
     @products = Product.all
+    @allTotalPrice = 0
+    @products.each do |product|
+      @allTotalPrice += product.price
+    end
   end
   
 end
